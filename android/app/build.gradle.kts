ea95d00b93ee8e3fd567de849d7fa2f0cab31041
Raine92 <raippe_92@hotmail.com>
2026-02-18 12:41:45 +0200

    Android-käännökset kuntoon

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -14,6 +14,9 @@ android {
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
+        // LISÄTTY: Mahdollistaa uusien Java-ominaisuuksien tuen vanhemmille laitteille
+        isCoreLibraryDesugaringEnabled = true
+        
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
@@ -23,20 +26,18 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.tunti_pankki"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
+
+        // LISÄTTY: Estää "Too many methods" -virheen, joka tulee usein Firebasen kanssa
+        multiDexEnabled = true
     }
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
@@ -44,4 +45,9 @@ android {
 
 flutter {
     source = "../.."
+}
+
+// Etsi tämä kohta tiedoston lopusta ja päivitä se:
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 }
